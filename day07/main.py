2d29ad1e66267e39a03629bd2c3260ed6bbc63cd
Anton <anton.gundermann@gmx.de>
2022-12-11 15:22:20 +0100

    day 7 and 8

---
+++ day07/main.py
@@ -0,0 +1,53 @@
+import os
+
+with open('input.txt') as f:
+    tree = {}
+    for line in f:
+        line = line.strip()
+        if line.startswith("$"):
+            _, cmd, *dir = line.split()
+            if cmd == "cd":
+                path = dir[0]
+                if path == "/":
+                    cur_dir = path
+                else:
+                    cur_dir = os.path.normpath(os.path.join(cur_dir, path))
+                if cur_dir not in tree:
+                    tree[cur_dir] = []
+        else:
+            size, name = line.split()
+            if size != 'dir':
+                tree[cur_dir].append((name, int(size)))
+            else:
+                tree[cur_dir].append(
+                    os.path.normpath(os.path.join(cur_dir, name)))
+
+
+def calc_size(tree, dir):
+    size = 0
+    for d in tree[dir]:
+        if d in tree:
+            size += calc_size(tree, d)
+        else:
+            size += d[1]
+    return size
+
+
+def solve(tree):
+    dir_sizes = {}
+    for dir in tree:
+        dir_sizes[dir] = calc_size(tree, dir)
+
+    # Part 1
+    p1 = sum([s for s in dir_sizes.values() if s < 100_000])
+
+    # Part 2
+    disk_size = 70_000_000
+    upadte_size = 30_000_000
+    needed_space = upadte_size - (disk_size - dir_sizes['/'])
+    p2 = sorted(s for s in dir_sizes.values() if s > needed_space)[0]
+
+    return p1, p2
+
+
+print(solve(tree))
---
+++ day08/main.py
@@ -0,0 +1,44 @@
+with open('input.txt') as f:
+    m = {}
+    trees = [[int(i) for i in line.strip()] for line in f]
+    for i, r in enumerate(trees):
+        for j, c in enumerate(r):
+            m[(i, j)] = c
+
+DIR = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+
+def is_visible(m, pos):
+    for xi, yi in DIR:
+        dir = []
+        new_pos = (pos[0] + xi, pos[1] + yi)
+        while new_pos in m:
+            dir.append(new_pos)
+            new_pos = (new_pos[0] + xi, new_pos[1] + yi)
+        if all(m[p] < m[pos] for p in dir):
+            return True
+    return False
+
+
+def scenic_score(m, pos):
+    ss = 1
+    for xi, yi in DIR:
+        score = 0
+        new_pos = (pos[0] + xi, pos[1] + yi)
+        found_block = False
+        while new_pos in m and not found_block:
+            score += 1
+            if m[new_pos] < m[pos]:
+                new_pos = (new_pos[0] + xi, new_pos[1] + yi)
+            else:
+                found_block = True
+
+        ss *= score
+    return ss
+
+
+def solve(m):
+    return sum(is_visible(m, p) for p in m), max(scenic_score(m, p) for p in m)
+
+
+print(solve(m))

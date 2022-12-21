6b2fe3692e31c6329cb390c242549c3ef6e83f2f
Anton Gundermann <anton.gundermann@gmx.de>
2022-12-21 22:41:09 +0100

    solved day21 part2

--- day21/main.py
+++ day21/main.py
@@ -1,36 +1,40 @@
+import sympy
+from sympy import solve as s
+
+
 def main():
     data = dict()
     with open('input.txt') as f:
         for line in f:
             name, operation = line.strip().split(': ')
-            data[name] = int(operation) if operation.isdigit() else operation
-    solve(data)
+            data[name] = operation
+    print(solve(data))
 
 
 def solve(data):
-    # print(calc_mokeys('root', data))
-    num1, _, num2 = data['root'].split()
-    print(find_path(num1, data))
+    part1 = int(eval(calc_mokeys('root', data, False)))
+
+    # Part2
+    data['humn'] = 'x'
+    left, _, right = data['root'].split()
+
+    left = calc_mokeys(left, data, True)
+    right = calc_mokeys(right, data, True)
+    humn_side = left if 'x' in left else right
+    other_side = left if 'x' not in left else right
+
+    part2 = int(s(f"{humn_side} - {other_side}")[0])
+
+    return part1, part2
 
 
-def calc_mokeys(node, data):
-    if isinstance(data[node], int):
+def calc_mokeys(node, data, part2):
+    if data[node].isdigit():
         return data[node]
+    if node == 'humn' and part2:
+        return 'x'
     val1, op, val2 = data[node].split()
-
-    # print(f"{calc_mokeys(val1, data)} {op} {calc_mokeys(val2, data)}")
-    return int(eval(f"{calc_mokeys(val1, data)} {op} {calc_mokeys(val2, data)}"))
-
-
-def find_path(node, data):
-    if isinstance(data[node], int):
-        # print(data[node], end="")
-        return str(data[node])
-
-    val1, op, val2 = data[node].split()
-    print(node + "\n\t" + find_path(val1, data) + op + find_path(val2, data))
-    return find_path(val1, data) + op + find_path(val2, data)
-
+    return f"({calc_mokeys(val1, data, part2)} {op} {calc_mokeys(val2, data, part2)})"
 
 
 if __name__ == '__main__':

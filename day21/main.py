d7cafba6d938fe7b188bd7b46cac54edcfce27b0
Anton Gundermann <anton.gundermann@gmx.de>
2022-12-21 20:44:21 +0100

    refactor

--- day21/main.py
+++ day21/main.py
@@ -8,15 +8,29 @@ def main():
 
 
 def solve(data):
-    print(calc_mokeys('root', data))
+    # print(calc_mokeys('root', data))
+    num1, _, num2 = data['root'].split()
+    print(find_path(num1, data))
 
 
 def calc_mokeys(node, data):
     if isinstance(data[node], int):
         return data[node]
     val1, op, val2 = data[node].split()
+
     # print(f"{calc_mokeys(val1, data)} {op} {calc_mokeys(val2, data)}")
     return int(eval(f"{calc_mokeys(val1, data)} {op} {calc_mokeys(val2, data)}"))
+
+
+def find_path(node, data):
+    if isinstance(data[node], int):
+        # print(data[node], end="")
+        return str(data[node])
+
+    val1, op, val2 = data[node].split()
+    print(node + "\n\t" + find_path(val1, data) + op + find_path(val2, data))
+    return find_path(val1, data) + op + find_path(val2, data)
+
 
 
 if __name__ == '__main__':

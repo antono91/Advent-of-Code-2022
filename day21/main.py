e891d8c382e47524fc9a7b715c1aa3eb0f437ca5
Anton Gundermann <anton.gundermann@gmx.de>
2022-12-21 13:51:22 +0100

    solved day21 part1

--- day21/main.py
+++ day21/main.py
@@ -1,5 +1,22 @@
 def main():
-    pass
+    data = dict()
+    with open('input.txt') as f:
+        for line in f:
+            name, operation = line.strip().split(': ')
+            data[name] = int(operation) if operation.isdigit() else operation
+    solve(data)
+
+
+def solve(data):
+    print(calc_mokeys('root', data))
+
+
+def calc_mokeys(node, data):
+    if isinstance(data[node], int):
+        return data[node]
+    val1, op, val2 = data[node].split()
+    # print(f"{calc_mokeys(val1, data)} {op} {calc_mokeys(val2, data)}")
+    return int(eval(f"{calc_mokeys(val1, data)} {op} {calc_mokeys(val2, data)}"))
 
 
 if __name__ == '__main__':

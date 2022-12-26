b71c9f5b9f798f6de3bdda72f80f01167fe0717f
Anton Gundermann <anton.gundermann@gmx.de>
2022-12-26 15:36:09 +0100

    day22 to ask on reddit

--- day22/main.py
+++ day22/main.py
@@ -1,9 +1,13 @@
+#!/usr/local/bin/python
+
+
 import re
 
 
 def main():
-    print(solve(*parse_input('input.txt'), True))
-    print(solve(*parse_input('input.txt'), False))
+    m, path = parse_input('input.txt')
+    print(solve(m, path, True))
+    print(solve(m, path, False))
 
 
 def parse_input(file):
@@ -11,75 +15,96 @@ def parse_input(file):
         m, path = f.read().split('\n\n')
         m = m.split('\n')
         grid = []
-        max_col = 0
         for line in m:
-            max_col = max(max_col, len(line))
-        for line in m:
-            if len(line) < max_col:
-                line += '_' * (max_col - len(line))
-            grid.append(['_' if c == ' ' else c for c in line])
-        path = [(int(x), d) for x, d in re.findall(r"(\d+)([A-Z])", path)]
+            if len(line) < 150:
+                line += ' ' * (150 - len(line))
+            grid.append([c for c in line])
+        path = re.findall(r"\d+|L|R", path)
     return grid, path
 
 
 def solve(grid, path, part1):
+    path_list = list()
     row, col = 0, grid[0].index('.')
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     d_pointer = 0
-    for steps, turn in path:
-        for _ in range(steps):
-            row, col = move(grid, directions[d_pointer], row, col, part1)
-        if turn == 'R':
-            d_pointer = (d_pointer + 1) % 4
-        elif turn == 'L':
-            d_pointer = (d_pointer - 1) % 4
-    final_col = col + 1
-    final_row = row + 1
-    return final_row * 1000 + final_col * 4 + d_pointer
+
+    for op in path:
+        if op.isdigit():
+            for s in range(int(op)):
+                row, col, d_pointer = move(grid, directions[d_pointer], row, col, part1)
+        else:
+            if op == 'R':
+                d_pointer = (d_pointer + 1) % 4
+            elif op == 'L':
+                d_pointer = (d_pointer - 1) % 4
+    # plot_grid(grid, path_list)
+    return (row + 1) * 1000 + (col + 1) * 4 + d_pointer
 
 
 # helper function
-def plot_grid(grid):
-    for row in grid:
-        line = ""
-        for c in row:
-            line += " " if c == "_" else c
-        print(line)
+def plot_grid(grid, path_list):
+    for y, x, ch in path_list:
+        grid[y][x] = ch
+        for j, row in enumerate(grid):
+            line = ""
+            for i, c in enumerate(row):
+                line += c
+            print(line)
 
 
 def move(grid, direction, row, col, part1):
     dr, dc = direction
-    nr = (row + dr) % len(grid)
-    nc = (col + dc) % len(grid[0])
+
     if part1:
-        while grid[nr][nc] == '_':
+        nr = (row + dr) % len(grid)
+        nc = (col + dc) % len(grid[0])
+        while grid[nr][nc] == ' ':
             nr = (nr + dr) % len(grid)
             nc = (nc + dc) % len(grid[0])
     else:
-        if len(grid[0]) <= nc < 0 or len(grid) <= nr < 0 or grid[nr][nc] == '_':
-            find_next_face(row, col, dr, dc)
+        nr, nc = row + dr, col + dc
+        if len(grid[0]) <= nc or nc < 0 or len(grid) <= nr or nr < 0 or grid[nr][nc] == ' ':
+            nr, nc, dr, dc = find_next_face(row, col, dr, dc)
+
+    d_pointer = [(0, 1), (1, 0), (0, -1), (-1, 0)].index((dr, dc))
 
     if grid[nr][nc] == '#':
-        return row, col
-    return nr, nc
+        return row, col, d_pointer
+    return nr, nc, d_pointer
 
 
 def find_next_face(row, col, dr, dc):
     face_transition = {
-        1, (0, -1):
+        (1, 0, -1): (0, 1, lambda r, c: (149 - r, 0)),
+        (1, -1, 0): (0, 1, lambda r, c: (c - 50 + 150, 0)),
+        (2, 0, 1): (0, -1, lambda r, c: (149 - r, 99)),
+        (2, -1, 0): (-1, 0, lambda r, c: (199, c - 100)),
+        (2, 1, 0): (0, -1, lambda r, c: (c - 100 + 50, 99)),
+        (4, 0, 1): (-1, 0, lambda r, c: (49, r - 50 + 100)),
+        (4, 0, -1): (1, 0, lambda r, c: (100, r - 50)),
+        (6, 0, -1): (0, 1, lambda r, c: (49 - (r - 100), 50)),
+        (6, -1, 0): (0, 1, lambda r, c: (c + 50, 50)),
+        (7, 0, 1): (0, -1, lambda r, c: (49 - (r - 100), 149)),
+        (7, 1, 0): (0, -1, lambda r, c: (150 + (c - 50), 49)),
+        (9, 0, 1): (-1, 0, lambda r, c: (149, (r - 150) + 50)),
+        (9, 0, -1): (1, 0, lambda r, c: (0, r - 150 + 50)),
+        (9, 1, 0): (1, 0, lambda r, c: (0, c + 100))
     }
-    face = (row // 50) * 3 + col // 50
-
+    face = row // 50 * 3 + col // 50
+    dr, dc, f = face_transition[(face, dr, dc)]
+    nr, nc = f(row, col)
+    return nr, nc, dr, dc
 
 
 if __name__ == '__main__':
     main()
 
-#       A B
-#       C
-#     D E
-#     F
-#    A > B(>)    B > E(<)    C > B(^)    D > E(>)    E > B(<)    F > E(^)
-#    A < D(>)    B < A(<)    C < D(v)    D < A(>)    E < D(<)    F < A(v)
-#    A v C(v)    B v C(<)    C v E(v)    D v F(v)    E v F(<)    F v B(v)
-#    A ^ F(>)    B ^ F(^)    C ^ A(^)    D ^ C(>)    E ^ C(^)    F ^ D(^)
+    #       1 2
+    #       4
+    #     6 7
+    #     9
+    #    1 > 2(>)    2 > 7(<)    4 > 2(^)    6 > 7(>)    7 > 2(<)    9 > 7(^)
+    #    1 < 6(>)    2 < 1(<)    4 < 6(v)    6 < 1(>)    7 < 6(<)    9 < 1(v)
+    #    1 v 4(v)    2 v 4(<)    4 v 7(v)    6 v 9(v)    7 v 9(<)    9 v 2(v)
+    #    1 ^ 9(>)    2 ^ 9(^)    4 ^ 1(^)    6 ^ 4(>)    7 ^ 4(^)    9 ^ 6(^)

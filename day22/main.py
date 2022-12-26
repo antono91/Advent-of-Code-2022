8a3656ba304eb3173ad5ab2045b979edc35d9cb5
Anton Gundermann <anton.gundermann@gmx.de>
2022-12-26 20:50:33 +0100

    solved day 22 part 2

--- day22/main.py
+++ day22/main.py
@@ -1,6 +1,3 @@
-#!/usr/local/bin/python
-
-
 import re
 
 
@@ -26,52 +23,39 @@ def parse_input(file):
 def solve(grid, path, part1):
     path_list = list()
     row, col = 0, grid[0].index('.')
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    d_pointer = 0
+    dr, dc = 0, 1
 
     for op in path:
         if op.isdigit():
             for s in range(int(op)):
-                row, col, d_pointer = move(grid, directions[d_pointer], row, col, part1)
+                row, col, dr, dc = move(grid, dr, dc, row, col, part1)
         else:
             if op == 'R':
-                d_pointer = (d_pointer + 1) % 4
+                dr, dc = dc, -dr
             elif op == 'L':
-                d_pointer = (d_pointer - 1) % 4
-    # plot_grid(grid, path_list)
+                dr, dc = -dc, dr
+
+    d_pointer = [(0, 1), (1, 0), (0, -1), (-1, 0)].index((dr, dc))
     return (row + 1) * 1000 + (col + 1) * 4 + d_pointer
 
 
-# helper function
-def plot_grid(grid, path_list):
-    for y, x, ch in path_list:
-        grid[y][x] = ch
-        for j, row in enumerate(grid):
-            line = ""
-            for i, c in enumerate(row):
-                line += c
-            print(line)
-
-
-def move(grid, direction, row, col, part1):
-    dr, dc = direction
-
+def move(grid, dr, dc, row, col, part1):
     if part1:
         nr = (row + dr) % len(grid)
         nc = (col + dc) % len(grid[0])
+        ndr, ndc = dr, dc
         while grid[nr][nc] == ' ':
             nr = (nr + dr) % len(grid)
             nc = (nc + dc) % len(grid[0])
     else:
         nr, nc = row + dr, col + dc
+        ndr, ndc = dr, dc
         if len(grid[0]) <= nc or nc < 0 or len(grid) <= nr or nr < 0 or grid[nr][nc] == ' ':
-            nr, nc, dr, dc = find_next_face(row, col, dr, dc)
-
-    d_pointer = [(0, 1), (1, 0), (0, -1), (-1, 0)].index((dr, dc))
+            nr, nc, ndr, ndc = find_next_face(row, col, dr, dc)
 
     if grid[nr][nc] == '#':
-        return row, col, d_pointer
-    return nr, nc, d_pointer
+        return row, col, dr, dc
+    return nr, nc, ndr, ndc
 
 
 def find_next_face(row, col, dr, dc):
@@ -97,9 +81,34 @@ def find_next_face(row, col, dr, dc):
     return nr, nc, dr, dc
 
 
+# transition table for the example input
+def find_next_face2(row, col, dr, dc):
+    face_transition = {
+        (2, 0, 1): (0, -1, lambda r, c: (11 - r, 15)),
+        (2, 0, -1): (1, 0, lambda r, c: (5, 4 + r)),
+        (2, -1, 0): (-1, 0, lambda r, c: (5, 3 - (8 - c))),
+        (4, -1, 0): (1, 0, lambda r, c: (0, 8 + (3 - c))),
+        (4, 0, -1): (-1, 0, lambda r, c: (11, 15 - (4 - r))),
+        (4, 1, 0): (-1, 0, lambda r, c: (11, 8 + (3 - c))),
+        (5, -1, 0): (0, 1, lambda r, c: ((c - 4), 8)),
+        (5, 1, 0): (0, 1, lambda r, c: (8 + (7 - c), 8)),
+        (6, 0, 1): (1, 0, lambda r, c: (8, 15 - (r - 4))),
+        (10, 0, -1): (-1, 0, lambda r, c: (7, 8 - (r - 8))),
+        (10, 1, 0): (-1, 0, lambda r, c: (7, (11 - c))),
+        (11, -1, 0): (0, -1, lambda r, c: (4 + (15 - c), 11)),
+        (11, 0, 1): (0, -1, lambda r, c: (11 - r, 11)),
+        (11, 1, 0): (0, 1, lambda r, c: (4 + (15 - r), 0)),
+    }
+    face = ((row // 4) * 4) + (col // 4)
+    dr, dc, f = face_transition[(face, dr, dc)]
+    nr, nc = f(row, col)
+    return nr, nc, dr, dc
+
+
 if __name__ == '__main__':
     main()
 
+    # Map for the real input
     #       1 2
     #       4
     #     6 7
@@ -108,3 +117,12 @@ if __name__ == '__main__':
     #    1 < 6(>)    2 < 1(<)    4 < 6(v)    6 < 1(>)    7 < 6(<)    9 < 1(v)
     #    1 v 4(v)    2 v 4(<)    4 v 7(v)    6 v 9(v)    7 v 9(<)    9 v 2(v)
     #    1 ^ 9(>)    2 ^ 9(^)    4 ^ 1(^)    6 ^ 4(>)    7 ^ 4(^)    9 ^ 6(^)
+
+    # Map for the example input
+    #         2
+    #     4 5 6
+    #         10 11
+    #
+    #    2 > 11(<)   4 ^ 2(v)    5 ^ 2(>)    6 > 11(v)    10 < 5(^)    11 ^ 6(<)
+    #    2 < 5(v)    4 < 11(^)   5 v 10(>)                10 v 4(^)    11 > 2(<)
+    #   ?2 ^ 4(v)    4 v 10(^)                                         11 v 4(>)
